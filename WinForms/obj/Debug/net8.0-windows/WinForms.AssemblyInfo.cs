526a1177f9b2a1118e502c0048327073bc100361
Soffesso <Soffess@hotmail.com>
2023-12-18 21:10:08 +0100

    added demo folder with images

--- WinForms/obj/Debug/net8.0-windows/WinForms.AssemblyInfo.cs
+++ WinForms/obj/Debug/net8.0-windows/WinForms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67e893d0ad55c81639b22d4ac4d12816ae3887c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e6a27efd071c4b366b9405fd9981fe932d70cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
